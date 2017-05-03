f965ab7cad22a15302161ecd6e591fb8e9b5a986
0-whitehand-0 <saul@cryptolab.net>
2017-05-03 15:40:10 +0200

    Fixed indentation

--- main.go
+++ main.go
@@ -1,211 +1,211 @@
 package main
 
 import (
-      "os"
-      "io/ioutil"
-      "encoding/json"
-      "log"
-      "github.com/go-telegram-bot-api/telegram-bot-api"
-      )
+	 "os"
+	 "io/ioutil"
+	 "encoding/json"
+	 "log"
+	 "github.com/go-telegram-bot-api/telegram-bot-api"
+)
 
 type d_transmission struct{
-   Url   string   `json:"url"`
-   Username string   `json:"username"`
-   Password string   `json:"password"`
+	 Url   string   `json:"url"`
+	 Username string   `json:"username"`
+	 Password string   `json:"password"`
 }
 
 type config struct {
-   Token          string     `json:"token"`
-   AllowedIDs     []int      `json:"allowed_ids"`
-   Transmission   d_transmission     `json:"transmission"`
+	 Token          string     `json:"token"`
+	 AllowedIDs     []int      `json:"allowed_ids"`
+	 Transmission   d_transmission     `json:"transmission"`
 }
 
 type cmdConfigs struct {
-   Yts   CmdConfigYts
-   Transmission   CmdConfigTransmission
+	 Yts   CmdConfigYts
+	 Transmission   CmdConfigTransmission
 }
 
 var Commandssl cmdConfigs
 
 func main() {
-   if len(os.Args) != 2 {
-      log.Fatalln("Usage: ./ytsbot <CONFIGFILE>")
-   }
-   cfg, err := parseConfig(os.Args[1])
-   if err != nil{
-      log.Fatalln(err)
-   }
-   bot, err := tgbotapi.NewBotAPI(cfg.Token)
-      if err != nil {
-         log.Panic(err)
-      }
-      NewCmdYts(&Commandssl.Yts)
-      NewCmdTransmission(&Commandssl.Transmission, cfg.Transmission.Url,cfg.Transmission.Username,cfg.Transmission.Password)
+	 if len(os.Args) != 2 {
+			log.Fatalln("Usage: ./ytsbot <CONFIGFILE>")
+	 }
+	 cfg, err := parseConfig(os.Args[1])
+	 if err != nil{
+			log.Fatalln(err)
+	 }
+	 bot, err := tgbotapi.NewBotAPI(cfg.Token)
+	 if err != nil {
+			log.Panic(err)
+	 }
+	 NewCmdYts(&Commandssl.Yts)
+	 NewCmdTransmission(&Commandssl.Transmission, cfg.Transmission.Url,cfg.Transmission.Username,cfg.Transmission.Password)
 
-      bot.Debug = false
-      log.Printf("Authorized on account %s", bot.Self.UserName)
+	 bot.Debug = false
+	 log.Printf("Authorized on account %s", bot.Self.UserName)
 
-      u := tgbotapi.NewUpdate(0)
-      u.Timeout = 60
+	 u := tgbotapi.NewUpdate(0)
+	 u.Timeout = 60
 
-      updates, err := bot.GetUpdatesChan(u)
-      for update := range updates {
-         go handle_updates(bot, update, cfg)
-      }
+	 updates, err := bot.GetUpdatesChan(u)
+	 for update := range updates {
+			go handle_updates(bot, update, cfg)
+	 }
 }
 
 func handle_updates(bot *tgbotapi.BotAPI, update tgbotapi.Update, cfg config) {
-   if update.Message!=nil{
-      if !allowed_ids(update.Message.From.ID, cfg.AllowedIDs){
-        log.Println("User " + update.Message.From.FirstName + " blocked! Alias: " + update.Message.From.UserName)
-        return
-        }
-      log.Printf("Message -> [%s] %s (id: %d, alias: %s)", update.Message.From.FirstName, update.Message.Text, update.Message.Chat.ID, update.Message.From.UserName)
-         var msg tgbotapi.MessageConfig
-         if Commandssl.Yts.Reg_rand.MatchString(update.Message.Text){
-            YtsSetPage(update.Message.Text)
-               msg = YtsNewKeyboard(update.Message.Chat.ID)
-         }else if Commandssl.Yts.Reg_detail.MatchString(update.Message.Text){
-            img, m, torrent, err := YtsDetail(update.Message.Text)
-               if err != nil{
-                  log.Println(err)
-                     return
-               }
-            if img != ""{
-               msg = tgbotapi.NewMessage(update.Message.Chat.ID, img)
-                  msg.DisableWebPagePreview = false
-                  bot.Send(msg)
-         }
-         msg = tgbotapi.NewMessage(update.Message.Chat.ID, m)
-            if torrent != ""{
-               var DownloadKeyboard = tgbotapi.NewInlineKeyboardMarkup(
-                     tgbotapi.NewInlineKeyboardRow(
-                        tgbotapi.NewInlineKeyboardButtonData("Download", "download_torrent_" + torrent),
-                        ),
-                     )
-                  msg.ReplyMarkup = DownloadKeyboard
-         }
-         }else if Commandssl.Yts.Reg_search.MatchString(update.Message.Text){
-            m, err := YtsSearch(update.Message.Text)
-               if err != nil{
-                  log.Println(err)
-                     return
-               }
-            msg = tgbotapi.NewMessage(update.Message.Chat.ID, m)
-         }else if Commandssl.Transmission.St_list == update.Message.Text{
-            m, err := BtList()
-               if err != nil{
-                  log.Println(err)
-                     return
-               }
-            msg = tgbotapi.NewMessage(update.Message.Chat.ID, m)
-         }else if Commandssl.Transmission.Reg_del.MatchString(update.Message.Text){
-            m, err := BtRemove(update.Message.Text)
-               if err != nil{
-                  log.Println(err)
-                     return
-               }
-            msg = tgbotapi.NewMessage(update.Message.Chat.ID, "Operation result: " + m)
-         }else{
-m := "Command not found!"
-      msg = tgbotapi.NewMessage(update.Message.Chat.ID, m)
-      msg.ReplyToMessageID = update.Message.MessageID
-         }
-      msg.DisableWebPagePreview = false
-         bot.Send(msg)
-   }else if update.CallbackQuery != nil {
-      if !allowed_ids(update.CallbackQuery.From.ID, cfg.AllowedIDs){
-        log.Println("User " + update.CallbackQuery.From.FirstName + " blocked! Alias: " + update.CallbackQuery.From.UserName)
-        return
-        }
-callback := tgbotapi.CallbackConfig{
-CallbackQueryID: update.CallbackQuery.ID,
-          }
-          log.Printf("Callback -> [%s] %s (id: %d, alias: %s)", update.CallbackQuery.From.FirstName,
-                update.CallbackQuery.Data, update.CallbackQuery.Message.Chat.ID, update.CallbackQuery.From.UserName)
-             if ((len(update.CallbackQuery.Data) > 3) && (update.CallbackQuery.Data[:4] == "YTS_")){
-                m, err := YtsRandom(update.CallbackQuery.Data[4:])
-                   if err != nil{
-                      log.Println(err)
-                         return
-                   }
-msg := tgbotapi.NewMessage(update.CallbackQuery.Message.Chat.ID, m)
-        var MoreKeyboard = tgbotapi.NewInlineKeyboardMarkup(
-              tgbotapi.NewInlineKeyboardRow(
-                 tgbotapi.NewInlineKeyboardButtonData("More...", update.CallbackQuery.Data),
-                 ),
-              )
-        msg.ReplyMarkup = MoreKeyboard
-        bot.Send(msg)
-             }else if ((len(update.CallbackQuery.Data) > 20) && (update.CallbackQuery.Data[:17] == "download_torrent_")){
-                /*var DoneKeyboard = tgbotapi.NewInlineKeyboardMarkup(
-                  tgbotapi.NewInlineKeyboardRow(
-                  tgbotapi.NewInlineKeyboardButtonData("Done!", ""),
-                  ),
-                  )
-                //msg := tgbotapi.NewEditMessageReplyMarkup(update.CallbackQuery.Message.Chat.ID,update.CallbackQuery.Message.MessageID , DoneKeyboard)
-msg :=
-tgbotapi.NewEditMessageCaption(update.CallbackQuery.Message.Chat.ID,update.CallbackQuery.Message.MessageID,"LEL")
-bot.Send(msg)*/
-                callback.Text = "Downloading movie..." + BtDown(update.CallbackQuery.Data[17:])
-                   callback.ShowAlert = true
-             }
-          if _, err := bot.AnswerCallbackQuery(callback); err != nil {
-             log.Println(err)
-          }
-   }else{
-      log.Println("Unknown query received: ", update)
-   }
+	 if update.Message!=nil{
+			if !allowed_ids(update.Message.From.ID, cfg.AllowedIDs){
+				 log.Println("User " + update.Message.From.FirstName + " blocked! Alias: " + update.Message.From.UserName)
+				 return
+			}
+			log.Printf("Message -> [%s] %s (id: %d, alias: %s)", update.Message.From.FirstName, update.Message.Text, update.Message.Chat.ID, update.Message.From.UserName)
+			var msg tgbotapi.MessageConfig
+			if Commandssl.Yts.Reg_rand.MatchString(update.Message.Text){
+				 YtsSetPage(update.Message.Text)
+				 msg = YtsNewKeyboard(update.Message.Chat.ID)
+			}else if Commandssl.Yts.Reg_detail.MatchString(update.Message.Text){
+				 img, m, torrent, err := YtsDetail(update.Message.Text)
+				 if err != nil{
+						log.Println(err)
+						return
+				 }
+				 if img != ""{
+						msg = tgbotapi.NewMessage(update.Message.Chat.ID, img)
+						msg.DisableWebPagePreview = false
+						bot.Send(msg)
+				 }
+				 msg = tgbotapi.NewMessage(update.Message.Chat.ID, m)
+				 if torrent != ""{
+						var DownloadKeyboard = tgbotapi.NewInlineKeyboardMarkup(
+							 tgbotapi.NewInlineKeyboardRow(
+									tgbotapi.NewInlineKeyboardButtonData("Download", "download_torrent_" + torrent),
+							 ),
+						)
+						msg.ReplyMarkup = DownloadKeyboard
+				 }
+			}else if Commandssl.Yts.Reg_search.MatchString(update.Message.Text){
+				 m, err := YtsSearch(update.Message.Text)
+				 if err != nil{
+						log.Println(err)
+						return
+				 }
+				 msg = tgbotapi.NewMessage(update.Message.Chat.ID, m)
+			}else if Commandssl.Transmission.St_list == update.Message.Text{
+				 m, err := BtList()
+				 if err != nil{
+						log.Println(err)
+						return
+				 }
+				 msg = tgbotapi.NewMessage(update.Message.Chat.ID, m)
+			}else if Commandssl.Transmission.Reg_del.MatchString(update.Message.Text){
+				 m, err := BtRemove(update.Message.Text)
+				 if err != nil{
+						log.Println(err)
+						return
+				 }
+				 msg = tgbotapi.NewMessage(update.Message.Chat.ID, "Operation result: " + m)
+			}else{
+				 m := "Command not found!"
+				 msg = tgbotapi.NewMessage(update.Message.Chat.ID, m)
+				 msg.ReplyToMessageID = update.Message.MessageID
+			}
+			msg.DisableWebPagePreview = false
+			bot.Send(msg)
+	 }else if update.CallbackQuery != nil {
+			if !allowed_ids(update.CallbackQuery.From.ID, cfg.AllowedIDs){
+				 log.Println("User " + update.CallbackQuery.From.FirstName + " blocked! Alias: " + update.CallbackQuery.From.UserName)
+				 return
+			}
+			callback := tgbotapi.CallbackConfig{
+				 CallbackQueryID: update.CallbackQuery.ID,
+			}
+			log.Printf("Callback -> [%s] %s (id: %d, alias: %s)", update.CallbackQuery.From.FirstName,
+			update.CallbackQuery.Data, update.CallbackQuery.Message.Chat.ID, update.CallbackQuery.From.UserName)
+			if ((len(update.CallbackQuery.Data) > 3) && (update.CallbackQuery.Data[:4] == "YTS_")){
+				 m, err := YtsRandom(update.CallbackQuery.Data[4:])
+				 if err != nil{
+						log.Println(err)
+						return
+				 }
+				 msg := tgbotapi.NewMessage(update.CallbackQuery.Message.Chat.ID, m)
+				 var MoreKeyboard = tgbotapi.NewInlineKeyboardMarkup(
+						tgbotapi.NewInlineKeyboardRow(
+							 tgbotapi.NewInlineKeyboardButtonData("More...", update.CallbackQuery.Data),
+						),
+				 )
+				 msg.ReplyMarkup = MoreKeyboard
+				 bot.Send(msg)
+			}else if ((len(update.CallbackQuery.Data) > 20) && (update.CallbackQuery.Data[:17] == "download_torrent_")){
+				 /*var DoneKeyboard = tgbotapi.NewInlineKeyboardMarkup(
+						tgbotapi.NewInlineKeyboardRow(
+							 tgbotapi.NewInlineKeyboardButtonData("Done!", ""),
+						),
+				 )
+				 //msg := tgbotapi.NewEditMessageReplyMarkup(update.CallbackQuery.Message.Chat.ID,update.CallbackQuery.Message.MessageID , DoneKeyboard)
+				 msg :=
+				 tgbotapi.NewEditMessageCaption(update.CallbackQuery.Message.Chat.ID,update.CallbackQuery.Message.MessageID,"LEL")
+				 bot.Send(msg)*/
+				 callback.Text = "Downloading movie..." + BtDown(update.CallbackQuery.Data[17:])
+				 callback.ShowAlert = true
+			}
+			if _, err := bot.AnswerCallbackQuery(callback); err != nil {
+				 log.Println(err)
+			}
+	 }else{
+			log.Println("Unknown query received: ", update)
+	 }
 }
 
 func YtsNewKeyboard(id int64) tgbotapi.MessageConfig {
 
-   var CategoryKeyboard = tgbotapi.NewInlineKeyboardMarkup(
-         tgbotapi.NewInlineKeyboardRow(
-            tgbotapi.NewInlineKeyboardButtonData("Action", "YTS_Action"),
-            tgbotapi.NewInlineKeyboardButtonData("Adventure", "YTS_Adventure"),
-            tgbotapi.NewInlineKeyboardButtonData("Animation", "YTS_Animation"),
-            ),
-         tgbotapi.NewInlineKeyboardRow(
-            tgbotapi.NewInlineKeyboardButtonData("Comedy", "YTS_Comedy"),
-            tgbotapi.NewInlineKeyboardButtonData("Documentary", "YTS_Documentary"),
-            tgbotapi.NewInlineKeyboardButtonData("Drama", "YTS_Drama"),
-            ),
-         tgbotapi.NewInlineKeyboardRow(
-            tgbotapi.NewInlineKeyboardButtonData("Fantasy", "YTS_Fantasy"),
-            tgbotapi.NewInlineKeyboardButtonData("Horror", "YTS_Horror"),
-            tgbotapi.NewInlineKeyboardButtonData("Mystery", "YTS_Mystery"),
-            ),
-         tgbotapi.NewInlineKeyboardRow(
-            tgbotapi.NewInlineKeyboardButtonData("Sport", "YTS_Sport"),
-            tgbotapi.NewInlineKeyboardButtonData("Sci-Fi", "YTS_Sci-fi"),
-            tgbotapi.NewInlineKeyboardButtonData("Thriller", "YTS_Thriller"),
-            ),
-         tgbotapi.NewInlineKeyboardRow(
-               tgbotapi.NewInlineKeyboardButtonData("All", "YTS_"),
-               ),
-         )
-            msg := tgbotapi.NewMessage(id, "Select a category:")
-            msg.ReplyMarkup = CategoryKeyboard
-            return msg
+	 var CategoryKeyboard = tgbotapi.NewInlineKeyboardMarkup(
+			tgbotapi.NewInlineKeyboardRow(
+				 tgbotapi.NewInlineKeyboardButtonData("Action", "YTS_Action"),
+				 tgbotapi.NewInlineKeyboardButtonData("Adventure", "YTS_Adventure"),
+				 tgbotapi.NewInlineKeyboardButtonData("Animation", "YTS_Animation"),
+			),
+			tgbotapi.NewInlineKeyboardRow(
+				 tgbotapi.NewInlineKeyboardButtonData("Comedy", "YTS_Comedy"),
+				 tgbotapi.NewInlineKeyboardButtonData("Documentary", "YTS_Documentary"),
+				 tgbotapi.NewInlineKeyboardButtonData("Drama", "YTS_Drama"),
+			),
+			tgbotapi.NewInlineKeyboardRow(
+				 tgbotapi.NewInlineKeyboardButtonData("Fantasy", "YTS_Fantasy"),
+				 tgbotapi.NewInlineKeyboardButtonData("Horror", "YTS_Horror"),
+				 tgbotapi.NewInlineKeyboardButtonData("Mystery", "YTS_Mystery"),
+			),
+			tgbotapi.NewInlineKeyboardRow(
+				 tgbotapi.NewInlineKeyboardButtonData("Sport", "YTS_Sport"),
+				 tgbotapi.NewInlineKeyboardButtonData("Sci-Fi", "YTS_Sci-fi"),
+				 tgbotapi.NewInlineKeyboardButtonData("Thriller", "YTS_Thriller"),
+			),
+			tgbotapi.NewInlineKeyboardRow(
+				 tgbotapi.NewInlineKeyboardButtonData("All", "YTS_"),
+			),
+	 )
+	 msg := tgbotapi.NewMessage(id, "Select a category:")
+	 msg.ReplyMarkup = CategoryKeyboard
+	 return msg
 }
 
 func parseConfig(file string) (config, error) {
-   b, err := ioutil.ReadFile(file)
-      if err != nil {
-         return config{}, err
-      }
-   var cfg config
-      if err := json.Unmarshal(b, &cfg); err != nil {
-         return config{}, err
-      }
-   log.Println("Config: ", cfg)
-      return cfg, nil
+	 b, err := ioutil.ReadFile(file)
+	 if err != nil {
+			return config{}, err
+	 }
+	 var cfg config
+	 if err := json.Unmarshal(b, &cfg); err != nil {
+			return config{}, err
+	 }
+	 log.Println("Config: ", cfg)
+	 return cfg, nil
 }
 
 func allowed_ids(a int, b []int) bool{
-   for _, i := range b{
-      if i == a{return true}
-   }
-   return false
+	 for _, i := range b{
+			if i == a{return true}
+	 }
+	 return false
 }
--- ytsag.go
+++ ytsag.go
@@ -1,213 +1,213 @@
 package main
 
 import (
-      "encoding/json"
-      "io/ioutil"
-      "net/http"
-      "net/url"
-      "regexp"
-      "errors"
-      "strconv"
-      "strings"
-      )
+	 "encoding/json"
+	 "io/ioutil"
+	 "net/http"
+	 "net/url"
+	 "regexp"
+	 "errors"
+	 "strconv"
+	 "strings"
+)
 
 type CmdConfigYts struct {
-   Reg_rand        *regexp.Regexp
-      Reg_detail      *regexp.Regexp
-      Reg_search        *regexp.Regexp
+	 Reg_rand        *regexp.Regexp
+	 Reg_detail      *regexp.Regexp
+	 Reg_search        *regexp.Regexp
 }
 
 var Yts_randof int
 
 func NewCmdYts(config *CmdConfigYts) {
-   config.Reg_rand = regexp.MustCompile(`^/random_yts(?:(@[a-zA-Z0-9_]+bot)?( [0-9]+)?$)`)
-      config.Reg_detail = regexp.MustCompile(`^/details_yts_[0-9]+$`)
-      config.Reg_search = regexp.MustCompile(`^/search_yts(?:(@[a-zA-Z0-9_]+bot)? .+$)`)
-      Yts_randof = 1
+	 config.Reg_rand = regexp.MustCompile(`^/random_yts(?:(@[a-zA-Z0-9_]+bot)?( [0-9]+)?$)`)
+	 config.Reg_detail = regexp.MustCompile(`^/details_yts_[0-9]+$`)
+	 config.Reg_search = regexp.MustCompile(`^/search_yts(?:(@[a-zA-Z0-9_]+bot)? .+$)`)
+	 Yts_randof = 1
 }
 
 func YtsSetPage(a string){
-b := strings.SplitN(a, " ", 2)
-      if len(b) == 2{
-         Yts_randof, _ = strconv.Atoi(b[1])
-      }else{
-         Yts_randof = 1
-      }
+	 b := strings.SplitN(a, " ", 2)
+	 if len(b) == 2{
+			Yts_randof, _ = strconv.Atoi(b[1])
+	 }else{
+			Yts_randof = 1
+	 }
 }
 
 func YtsRandom(gen string) (string, error){
-   type movie struct{
-      ID int   `json:"id"`
-         Title string   `json:"title_long"`
-         Rating   float32  `json:"rating"`
-         Genres   []string `json:"genres"`
-   }
-   type t_data struct{
-      Movie_count int   `json:"movie_count"`
-         Movies   []movie  `json:"movies"`
-   }
-   var respData struct {
-      Data t_data `json:"data"`
-   }
+	 type movie struct{
+			ID int   `json:"id"`
+			Title string   `json:"title_long"`
+			Rating   float32  `json:"rating"`
+			Genres   []string `json:"genres"`
+	 }
+	 type t_data struct{
+			Movie_count int   `json:"movie_count"`
+			Movies   []movie  `json:"movies"`
+	 }
+	 var respData struct {
+			Data t_data `json:"data"`
+	 }
 
-limit := 5
-          qResp, err := YtsQuery(limit, "", Yts_randof, "1080p", "4", gen)
-          if err != nil{
-             return "", err
-          }
-       err = json.Unmarshal(qResp, &respData)
-          if err != nil {
-             return "", err
-          }
-       if respData.Data.Movie_count == 0{
-          return "No movies found!", nil
-       }
-       if gen == ""{gen = "All"}
-m := "Page: " + strconv.Itoa(Yts_randof) + " - Category: " + gen + "\n"
-      if Yts_randof*limit >= respData.Data.Movie_count{
-         Yts_randof = 1
-      }else{
-         Yts_randof++
-      }
+	 limit := 5
+	 qResp, err := YtsQuery(limit, "", Yts_randof, "1080p", "4", gen)
+	 if err != nil{
+			return "", err
+	 }
+	 err = json.Unmarshal(qResp, &respData)
+	 if err != nil {
+			return "", err
+	 }
+	 if respData.Data.Movie_count == 0{
+			return "No movies found!", nil
+	 }
+	 if gen == ""{gen = "All"}
+	 m := "Page: " + strconv.Itoa(Yts_randof) + " - Category: " + gen + "\n"
+	 if Yts_randof*limit >= respData.Data.Movie_count{
+			Yts_randof = 1
+	 }else{
+			Yts_randof++
+	 }
 
-   for _, i := range respData.Data.Movies{
-      s_genres := ": "
-      for _, j := range i.Genres{
-         s_genres += j + ", "
-      }
-      m += "\nTítulo: " + i.Title + "\nRating: " +
-         strconv.FormatFloat(float64(i.Rating), 'f', -1, 32) + "\nGenres" +
-         s_genres[:len(s_genres)-2] + "\nDetails and Download: /details_yts_" + strconv.Itoa(i.ID) + "\n"
-   }
-   return m, nil
+	 for _, i := range respData.Data.Movies{
+			s_genres := ": "
+			for _, j := range i.Genres{
+				 s_genres += j + ", "
+			}
+			m += "\nTítulo: " + i.Title + "\nRating: " +
+			strconv.FormatFloat(float64(i.Rating), 'f', -1, 32) + "\nGenres" +
+			s_genres[:len(s_genres)-2] + "\nDetails and Download: /details_yts_" + strconv.Itoa(i.ID) + "\n"
+	 }
+	 return m, nil
 }
 
 func YtsSearch(a string) (string, error) {
-   type movie struct{
-      ID int   `json:"id"`
-         Title string   `json:"title_long"`
-         Rating   float32  `json:"rating"`
-         Genres   []string `json:"genres"`
-   }
-   type t_data struct{
-      Movie_count int   `json:"movie_count"`
-         Movies   []movie  `json:"movies"`
-   }
-   var respData struct {
-      Data t_data `json:"data"`
-   }
+	 type movie struct{
+			ID int   `json:"id"`
+			Title string   `json:"title_long"`
+			Rating   float32  `json:"rating"`
+			Genres   []string `json:"genres"`
+	 }
+	 type t_data struct{
+			Movie_count int   `json:"movie_count"`
+			Movies   []movie  `json:"movies"`
+	 }
+	 var respData struct {
+			Data t_data `json:"data"`
+	 }
 
-b := strings.SplitN(a, " ", 2)
-      if len(b) != 2{
-         return "", errors.New("Error in search function")
-      }
-   qResp, err := YtsQuery(50, b[1], 1, "", "", "")
-      if err != nil{
-         return "", err
-      }
-   err = json.Unmarshal(qResp, &respData)
-      if err != nil {
-         return "", err
-      }
-   if respData.Data.Movie_count == 0{
-      return "No movies found!", nil
-   }
-m := ""
-   for _, i := range respData.Data.Movies{
-      s_genres := ": "
-      for _, j := range i.Genres{
-         s_genres += j + ", "
-      }
-      m += "\nTítulo: " + i.Title + "\nRating: " +
-         strconv.FormatFloat(float64(i.Rating), 'f', -1, 32) + "\nGenres" +
-         s_genres[:len(s_genres)-2] + "\nDetails and Download: /details_yts_" + strconv.Itoa(i.ID) + "\n"
-   }
-   return m, nil
+	 b := strings.SplitN(a, " ", 2)
+	 if len(b) != 2{
+			return "", errors.New("Error in search function")
+	 }
+	 qResp, err := YtsQuery(50, b[1], 1, "", "", "")
+	 if err != nil{
+			return "", err
+	 }
+	 err = json.Unmarshal(qResp, &respData)
+	 if err != nil {
+			return "", err
+	 }
+	 if respData.Data.Movie_count == 0{
+			return "No movies found!", nil
+	 }
+	 m := ""
+	 for _, i := range respData.Data.Movies{
+			s_genres := ": "
+			for _, j := range i.Genres{
+				 s_genres += j + ", "
+			}
+			m += "\nTítulo: " + i.Title + "\nRating: " +
+			strconv.FormatFloat(float64(i.Rating), 'f', -1, 32) + "\nGenres" +
+			s_genres[:len(s_genres)-2] + "\nDetails and Download: /details_yts_" + strconv.Itoa(i.ID) + "\n"
+	 }
+	 return m, nil
 }
 
 func YtsDetail(a string) (string,string,string,error){
-   type torrent struct{
-      Hash   string  `json:"hash"`
-   }
-   type movie struct{
-      ID    int      `json:"id"`
-         Description   string `json:"description_full"`
-         Rating float32  `json:"rating"`
-         Title string   `json:"title_long"`
-         Img   string   `json:"large_cover_image"`
-         Trailer  string   `json:"yt_trailer_code"`
-         Torrents  []torrent   `json:"torrents"`
-         Genres    []string `json:"genres"`
-   }
-   type t_data struct{
-      Movie   movie  `json:"movie"`
-   }
-   var respData struct {
-      Data t_data `json:"data"`
-   }
-b := strings.SplitN(a, "_", 3)
-      var id int
-      var err error
-      if len(b) == 3{
-         id, err = strconv.Atoi(b[2])
-            if err != nil{
-               return "","", "", err
-            }
-      }else{
-         return "", "", "", errors.New("Error parsing the command")
-      }
-   resp, err := http.Get("https://yts.ag/api/v2/movie_details.json?movie_id=" + strconv.Itoa(id))
-      if err != nil {
-         return "","", "",err
-      }
-   defer resp.Body.Close()
-      if resp.StatusCode != http.StatusOK {
-         return "","", "", errors.New("HTTP Status: " + strconv.Itoa(resp.StatusCode))
-      }
-   repBody, err := ioutil.ReadAll(resp.Body)
-      if err != nil {
-         return "","", "", err
-      }
-   err = json.Unmarshal(repBody, &respData)
-      if err != nil {
-         return "","", "", err
-      }
-   if respData.Data.Movie.ID == 0{
-      return "", "No movie found with this ID", "", nil
-   }
-   var s_torrent string
-      if len(respData.Data.Movie.Torrents) > 1{
-         s_torrent = respData.Data.Movie.Torrents[1].Hash
-      }else{
-         s_torrent = respData.Data.Movie.Torrents[0].Hash
-      }
-s_genres := ": "
-             for _, i := range respData.Data.Movie.Genres{
-                s_genres += i + ", "
-             }
-m := "+ Título: " + respData.Data.Movie.Title + "\n+ Descripción: " +
-      respData.Data.Movie.Description + "\n+ Géneros" + s_genres[:len(s_genres)-2] + "\n+ Rating: " +
-      strconv.FormatFloat(float64(respData.Data.Movie.Rating), 'f', -1, 32) +
-      "\n+ Trailer: " + "https://www.youtube.com/watch?v=" + respData.Data.Movie.Trailer
-      return respData.Data.Movie.Img, m, s_torrent, nil
+	 type torrent struct{
+			Hash   string  `json:"hash"`
+	 }
+	 type movie struct{
+			ID    int      `json:"id"`
+			Description   string `json:"description_full"`
+			Rating float32  `json:"rating"`
+			Title string   `json:"title_long"`
+			Img   string   `json:"large_cover_image"`
+			Trailer  string   `json:"yt_trailer_code"`
+			Torrents  []torrent   `json:"torrents"`
+			Genres    []string `json:"genres"`
+	 }
+	 type t_data struct{
+			Movie   movie  `json:"movie"`
+	 }
+	 var respData struct {
+			Data t_data `json:"data"`
+	 }
+	 b := strings.SplitN(a, "_", 3)
+	 var id int
+	 var err error
+	 if len(b) == 3{
+			id, err = strconv.Atoi(b[2])
+			if err != nil{
+				 return "","", "", err
+			}
+	 }else{
+			return "", "", "", errors.New("Error parsing the command")
+	 }
+	 resp, err := http.Get("https://yts.ag/api/v2/movie_details.json?movie_id=" + strconv.Itoa(id))
+	 if err != nil {
+			return "","", "",err
+	 }
+	 defer resp.Body.Close()
+	 if resp.StatusCode != http.StatusOK {
+			return "","", "", errors.New("HTTP Status: " + strconv.Itoa(resp.StatusCode))
+	 }
+	 repBody, err := ioutil.ReadAll(resp.Body)
+	 if err != nil {
+			return "","", "", err
+	 }
+	 err = json.Unmarshal(repBody, &respData)
+	 if err != nil {
+			return "","", "", err
+	 }
+	 if respData.Data.Movie.ID == 0{
+			return "", "No movie found with this ID", "", nil
+	 }
+	 var s_torrent string
+	 if len(respData.Data.Movie.Torrents) > 1{
+			s_torrent = respData.Data.Movie.Torrents[1].Hash
+	 }else{
+			s_torrent = respData.Data.Movie.Torrents[0].Hash
+	 }
+	 s_genres := ": "
+	 for _, i := range respData.Data.Movie.Genres{
+			s_genres += i + ", "
+	 }
+	 m := "+ Título: " + respData.Data.Movie.Title + "\n+ Descripción: " +
+	 respData.Data.Movie.Description + "\n+ Géneros" + s_genres[:len(s_genres)-2] + "\n+ Rating: " +
+	 strconv.FormatFloat(float64(respData.Data.Movie.Rating), 'f', -1, 32) +
+	 "\n+ Trailer: " + "https://www.youtube.com/watch?v=" + respData.Data.Movie.Trailer
+	 return respData.Data.Movie.Img, m, s_torrent, nil
 }
 
 
 func YtsQuery (limit int, term string, page int, quality string, rating string, genre string) ([]byte, error){
-   resp, err := http.Get("https://yts.ag/api/v2/list_movies.json?limit=" + strconv.Itoa(limit) +
-         "&page=" + strconv.Itoa(page) + "&query_term=" + url.QueryEscape(term) + "&quality=" + quality +
-         "&minimum_rating=" + rating + "&genre=" + genre + "&sort_by=year&order_by=desc")
+	 resp, err := http.Get("https://yts.ag/api/v2/list_movies.json?limit=" + strconv.Itoa(limit) +
+	 "&page=" + strconv.Itoa(page) + "&query_term=" + url.QueryEscape(term) + "&quality=" + quality +
+	 "&minimum_rating=" + rating + "&genre=" + genre + "&sort_by=year&order_by=desc")
 
-      if err != nil {
-         return []byte{}, err
-      }
-   defer resp.Body.Close()
-      if resp.StatusCode != http.StatusOK {
-         return []byte{}, errors.New("HTTP Status: " + strconv.Itoa(resp.StatusCode))
-      }
-   repBody, err := ioutil.ReadAll(resp.Body)
-      if err != nil {
-         return []byte{}, err
-      }
-   return repBody, nil
+	 if err != nil {
+			return []byte{}, err
+	 }
+	 defer resp.Body.Close()
+	 if resp.StatusCode != http.StatusOK {
+			return []byte{}, errors.New("HTTP Status: " + strconv.Itoa(resp.StatusCode))
+	 }
+	 repBody, err := ioutil.ReadAll(resp.Body)
+	 if err != nil {
+			return []byte{}, err
+	 }
+	 return repBody, nil
 }
